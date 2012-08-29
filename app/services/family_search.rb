ac94bcd3d41d31dbb68ffe3b15ca2ea387cb9630
Thorn731 <notdefined@list.ru>
2012-08-29 11:15:42 +0400

    tweaked search page

--- app/services/family_search.rb
+++ app/services/family_search.rb
@@ -8,8 +8,8 @@ class FamilySearch
     go_ids = par[:group_options_id_in].reject { |e| e == "" } if par[:group_options_id_in]
     object_to_search = (go_ids.nil? or go_ids.length.zero?) ? Family : Family.joins(:group_options).where(group_options: {id: go_ids}).group("families.id").having("count(families.id)= ?", go_ids.length)
     if (par[:date_from] or par[:date_to]) and (par[:help_type])
-      help_from = par[:date_from]
-      help_to   = par[:date_to]
+      help_from = par[:date_from] || Date.today
+      help_to   = par[:date_to] || Date.today
       ids_with_help = Family.includes(:family_helps).where("family_helps.made_at >= ? and family_helps.made_at <= ? and family_helps.help_type_id = ?", help_from, help_to, par[:help_type]).map(&:id) << -1
       par[:search][:id_not_in] = ids_with_help
     end
