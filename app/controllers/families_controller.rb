a92e2566bc47e86cf62d4721022393c2cc1c3875
Thorn731 <notdefined@list.ru>
2012-10-16 11:19:26 +0400

    showing whole children count

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -69,6 +69,7 @@ class FamiliesController < ApplicationController
     @families  = @search.relation.select("DISTINCT(families.id), families.*").page(params[:page]).per_page(100)
     @whole_families = @search.count
     @whole_people = @search.all.map(&:member_counter).compact.sum
+    @whole_children = @search.all.map(&:children_counter).compact.sum
     respond_to do |format|
       format.html
       format.xls do
