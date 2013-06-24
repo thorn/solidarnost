4359ac6f0cf5027e6a1bb899d598e5388e557ea2
Thorn731 <notdefined@list.ru>
2013-06-24 22:43:43 +0400

    removed sensitive data

--- config/recipes/set_environment.rb
+++ config/recipes/set_environment.rb
@@ -1,8 +1,7 @@
 namespace :environment_setup do
   desc "Setup environment variables for oauth"
   task :set, roles: :app do
-    template 'private/google1959137a931c9501.html', "#{current_path}/public/google1959137a931c9501.html"
-    template 'private/environment_variables.rb.erb', "#{current_path}/config/environment_variables.rb"
+    # template 'private/google1959137a931c9501.html', "#{current_path}/public/google1959137a931c9501.html"
   end
   after "deploy:cold", "environment_setup:set"
 end
