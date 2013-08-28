6a4d96b8b764fc9a590ed0ae1f29c38e36766184
arsen netdev plugdev sound <arsen@arsen-pc.net>
2013-08-28 14:26:55 +0400

    fixed visits search

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -48,11 +48,12 @@ class VisitsController < ApplicationController
   def search
     @search = Visit.search(params[:search])
     @visits = @search.page(params[:page]).per_page(100)
-    @whole_visits = @search.all.length
+    @whole_visits = @search.count
     if @visits.length > 0
-      @families = Family.where(id: @search.all.map(&:family_id).uniq)
-      @whole_people = @families.sum(:member_counter)
-      @whole_families = @families.count
+      @families = Family.where(id: @visits.map(&:family_id).uniq)
+      fam = Family.select("member_counter").where(id: @search.all.map(&:family_id).uniq)
+      @whole_people = fam.sum(:member_counter)
+      @whole_families = fam.count
     else
       @families = []
       @whole_people = 0
